d9a9988dfedbbbf48ba8f2b8f7f7a62d7d28b925
Katya <katya.tarasova97@yandex.ru>
2020-03-26 17:19:21 +0300

    second'

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+var leftBorderWidth = 1;
+let second = 2;
+let something;
+console.log(something);
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+//console.log(persone["name"]);
+//let arr= ['plum.png','orange.jpg', 'apple.bmp'];
+//console.log(arr[2]);
+//let answer = +prompt("Если вам 18?", "Да");
+//console.log(typeof(answer));
+//console.log("arr" + "-object");
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
